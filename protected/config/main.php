baf222dcafcf688a545c27501cf2151d41f78107
Larry Ullman <larry@larryullman.com>
2014-10-04 19:24:55 -0400

    enabled gii

--- protected/config/main.php
+++ protected/config/main.php
@@ -20,14 +20,12 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>false,
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
 	),
 
 	// application components
