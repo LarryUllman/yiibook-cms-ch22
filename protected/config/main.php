9c7d874878f6d754229a12d0239a409e1208f692
Larry Ullman <larry@larryullman.com>
2014-10-04 19:29:20 -0400

    enabled web log

--- protected/config/main.php
+++ protected/config/main.php
@@ -63,11 +63,9 @@ return array(
 					'levels'=>'error, warning',
 				),
 				// uncomment the following to show log messages on web pages
-				/*
 				array(
 					'class'=>'CWebLogRoute',
 				),
-				*/
 			),
 		),
 	),
