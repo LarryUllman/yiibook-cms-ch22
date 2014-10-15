f420822741d5e354b4d3332369590ff5a8e13091
Larry Ullman <larry@larryullman.com>
2014-10-14 22:52:50 -0400

    edit page form

--- protected/views/page/_form.php
+++ protected/views/page/_form.php
@@ -20,18 +20,6 @@
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'user_id'); ?>
-		<?php echo $form->textField($model,'user_id',array('size'=>10,'maxlength'=>10)); ?>
-		<?php echo $form->error($model,'user_id'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'live'); ?>
-		<?php echo $form->textField($model,'live'); ?>
-		<?php echo $form->error($model,'live'); ?>
-	</div>
-
-	<div class="row">
 		<?php echo $form->labelEx($model,'title'); ?>
 		<?php echo $form->textField($model,'title',array('size'=>60,'maxlength'=>100)); ?>
 		<?php echo $form->error($model,'title'); ?>
@@ -39,20 +27,20 @@
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'content'); ?>
-		<?php echo $form->textArea($model,'content',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->textArea($model,'content',array('rows'=>20, 'cols'=>100)); ?>
 		<?php echo $form->error($model,'content'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'date_updated'); ?>
-		<?php echo $form->textField($model,'date_updated'); ?>
-		<?php echo $form->error($model,'date_updated'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'date_published'); ?>
 		<?php echo $form->textField($model,'date_published'); ?>
 		<?php echo $form->error($model,'date_published'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'live'); ?>
+		<?php echo $form->checkBox($model,'live'); ?>
+		<?php echo $form->error($model,'live'); ?>
 	</div>
 
 	<div class="row buttons">
