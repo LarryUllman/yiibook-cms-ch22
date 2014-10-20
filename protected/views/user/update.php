7433cb2b1e73dbef7d696962071ae04010f07ef9
Larry Ullman <larry@larryullman.com>
2014-10-19 20:47:30 -0400

    change user update heading

--- protected/views/user/update.php
+++ protected/views/user/update.php
@@ -10,6 +10,6 @@ $this->breadcrumbs=array(
 
 ?>
 
-<h1>Update User <?php echo $model->id; ?></h1>
+<h1>Update User <?php echo $model->username; ?></h1>
 
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>
